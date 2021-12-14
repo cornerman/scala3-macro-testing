75f2aff452b46c500c4c24c468a9bfa0c5479df2
johannes karoff <johannes@karoff.net>
2021-12-14 20:10:02 +0100

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,30 @@
+package test
+
 trait Command {
   def run: Int
   def run2: Int
-  def ignored: String
+  def run3(i: Option[String])(j: Either[Int, String], k: String): Int
+  def run4[T]: Int
+  // def ignored: String
 }
 
 val commander = new Command {
   def run = 15
   def run2 = 10
   def ignored = "hallo"
+  def run3(i: Int)(j: String, k: String): String = "1"
 }
 val commander2 = new Command {
   def run = 0
   def run2 = 1
   def ignored = "peter"
+  def run3(i: Int)(j: String, k: String): String = "2"
 }
+
+class Bla
 
 @main
 def main = {
-  println(MyMacro.call[MyMacro.Test, Int](null, "run"))
-  // println(MyMacro.call[Command, Int](commander, "run"))
-  // println(MyMacro.call[Command, Int](commander2, "run"))
-  // println(MyMacro.call[Command, Int](commander, "run2"))
-  // println(MyMacro.call[Command, Int](commander2, "run2"))
-  // println(MyMacro.call[Command, String](commander, "ignored"))
-  // println(MyMacro.call[Command, String](commander2, "ignored"))
+  println(MyMacro.router[Command, Int](commander)("run")())
+  println(MyMacro.client[Bla, Int](() => 12))
 }
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,7 +1,10 @@
+package test
+
 import scala.quoted.*
 
 object MyMacro {
-  inline def call[T, R](t: T, key: String): R = ${MyMacro.nameImpl[T, R]('t, 'key)}
+  inline def router[T, R](t: T): Map[String, () => R] = ${MyMacro.routerImpl[T, R]('t)}
+  inline def client[T, R](r: () => R): T = ${MyMacro.clientImpl[T, R]('r)}
 
   // def definedMethodsInType(tpe: Type): List[(MethodSymbol, Type)] = for {
   //   member <- tpe.members.toList
@@ -43,30 +46,33 @@ object MyMacro {
   //    case (k, _) => Left(s"""method $k is overloaded (rename the method or add a @PathName("other-name"))""")
   //  }.toList
 
-  trait Upper {
-    def theonlyValidForNowInherited: AResult[Int]
+  def isExpectedReturnType[R: Type](using Quotes): quotes.reflect.Symbol => Boolean = { method =>
+    import quotes.reflect.*
+
+    val expectedReturnType = TypeRepr.of[R]
+
+    method.tree match {
+      case DefDef(_,_,typedTree,_) =>
+          TypeRepr.of(using typedTree.tpe.asType) <:< expectedReturnType
+    //     TypeRepr.of(using typedTree.tpe.asType) match {
+    //       case AppliedType(repr, _) => repr <:< expectedReturnType
+    //       case _ => false
+    //     }
+      case _ => false
+    }
   }
 
-  trait Test extends Upper {
-    def foo: Int
-    def fooWith(): Int
-    def foobar(s:String): Int
-    def heinz: String
+  ///TODO no overloads
+  def isValidMethod[R: Type](using q: Quotes): quotes.reflect.Symbol => Boolean = { method =>
+    val isExpectedReturnTypeFun = isExpectedReturnType[R]
 
-    def NOHeinz: Int = 1
-
-    def theonlyValidForNow: AResult[Int]
-    // def theonlyValidForNow2ButNotAbstract: AResult[Int] = null
-
-    def generic[T]: Int
-
-    private def NO: Int = 1
-    protected def NO2:Int
-    private[MyMacro] def NO3 :Int
+    !method.paramSymss.headOption.exists(_.exists(_.isType)) && //isGeneric
+      isExpectedReturnTypeFun(method)
   }
 
   def definedMethodsInType[T: Type](using Quotes): List[quotes.reflect.Symbol] = {
     import quotes.reflect.*
+
     val tree = TypeTree.of[T]
 
     for {
@@ -82,87 +88,161 @@ object MyMacro {
 
       if !member.isClassConstructor
       if !member.flags.is(Flags.Synthetic)
-    } yield {
-      // println(member)
-      println("===")
-      println(member.name)
-      println(member.flags.show)
-      println(member.getClass)
-      member
-    }
+    } yield member
   }
 
-  trait AResult[Warum] {
-  }
-
-  def nameImpl[T: Type, R: Type](t: Expr[T], key: Expr[String])(using Quotes): Expr[R] = {
+  def routerImpl[T: Type, R: Type](t: Expr[T])(using Quotes): Expr[Map[String, () => R]] = {
     import quotes.reflect.*
 
-    val Expr(keyString) = key
-
-    val repr = TypeRepr.of[T]
-
-    val expectedReturnType = TypeRepr.of[R]
-
     val methods = definedMethodsInType[T]
-
-    def typeOfSymbol(method: Symbol) = {
-      method.tree match {
-        case DefDef(_,_,typedTree,_) => typedTree.tpe.asType
-        case _ => ???
-      }
-    }
-
-    def isCorrectReturnType(method: Symbol) = {
-      method.tree match {
-        case DefDef(_,_,typedTree,_) =>
-            TypeRepr.of(using typedTree.tpe.asType) <:< expectedReturnType
-      //     TypeRepr.of(using typedTree.tpe.asType) match {
-      //       case AppliedType(repr, _) => repr <:< expectedReturnType
-      //       case _ => false
-      //     }
-        case _ => false
-      }
-    }
-
-    val validMethods = for {
-      method <- methods
-      if !method.paramSymss.headOption.exists(_.exists(_.isType)) //isGeneric
-      if isCorrectReturnType(method)
-      ///TODO no overloads
-    } yield (method.name, method)
-
-    val methodMap = validMethods.toMap
-
-    val method = methodMap.get(keyString) match {
-      case Some(method) => method
-      case _ => ??? //compiletime.error(" not found") //in type '${TypeRepr.of[T].termSymbol.name}': $keyString")
-    }
-    println(method)
-
-
-    method.paramSymss
-
-
-    // val validatedMethods = methods.map { symbol => validateMethod(expectedReturnType, sym, tpe) }
-    // val validatedType = eitherSeq(validatedMethods)
-    //   .flatMap(methods => eitherSeq(validateAllMethods(methods)))
-
-    // validatedType match {
-    //   case Right(methods) => methods
-    //   case Left(errors) => abort(s"type '$tpe' contains unsupported methods: ${errors.mkString(", ")}")
-    // }
-
-    val sym = Symbol.newMethod(repr.typeSymbol, method.name, TypeRepr.of(using typeOfSymbol(method)))
-    println(sym)
+    val validMethods = methods.filter(isValidMethod[R])
 
     val x = '{
       val value = ${t}
-      ${Select('{value}.asTerm, method).asExprOf[R]}
+      _root_.scala.collection.immutable.Map.from[String, () => R](${Expr.ofSeq(
+        validMethods.map { method =>
+          println(method.name)
+          val call = Select('{value}.asTerm, method)
+
+          val callWithArgs = method.paramSymss.foldLeft[Term](call)((accum, params) =>
+            Apply(accum, params.map(_ => '{null}.asTerm))
+          )
+
+          '{
+            ${Expr(method.name)} -> { () =>
+              ${callWithArgs.asExprOf[R]}
+            }
+          }
+        }
+      )})
     }
 
     println(x.show)
 
     x
   }
+
+  def clientImpl[T: Type, R: Type](r: Expr[() => R])(using Quotes): Expr[T] = {
+    import quotes.reflect.*
+
+    val apiType = TypeRepr.of[T]
+
+    val methods = definedMethodsInType[T]
+    val validMethods = methods.filter(isValidMethod[R])
+
+    val tree = TypeTree.of[T]
+
+    val aClassDef = '{ class Test }
+    val Inlined(_, _, Block(list, _)) = aClassDef.asTerm
+    // val aClassDef = '{ new AnyRef {
+    //   def foo = 1
+    // } }
+    val orig = list.head.asInstanceOf[TypeDef].symbol
+    val next = Symbol.classSymbol(apiType.typeSymbol.fullName)
+    // val x = TypeDef.copy(TypeDef(Symbol.classSymbol("Test")))("Bla", '{}.asTerm).asExpr
+
+    // println(x.asTerm)
+    // val x = 
+// // Block(
+// //   List(
+//     TypeDef(
+//       $anon,
+//       Template(
+//         DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),
+//         List(
+//           Apply(Select(New(Ident(AnyRef)),<init>),List())
+//         ),
+//         ValDef(_,EmptyTree,EmptyTree),
+//         List(
+//           DefDef(foo,List(),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Int)],Literal(Constant(1)))
+//         )
+//       )
+//     )
+  // ),
+  // Typed(
+  //   Apply(Select(New(Ident($anon)),<init>),List()),
+  //   TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)]
+  // )
+// )
+
+    // val x = '{
+    //   // ${TypeDef(
+    //   //   $anon,
+    //   //   Template(
+    //   //     DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),
+    //   //     List(Apply(Select(New(Ident(AnyRef)),<init>),List())),
+    //   //     ValDef(_,EmptyTree,EmptyTree),
+    //   //     List(DefDef(foobar,List(),Ident(Int),Literal(Constant(1))))
+    //   //   )
+    //   // )}
+    //   ${aClassDef.asExpr}
+    //   // ${
+    //   //   ClassDef
+    //   //   .copy(aClassDef)(
+    //   //     "Anon",
+    //   //     aClassDef.constructor,
+    //   //   parents = List.empty,
+    //   // selfOpt = None,
+    //   // body = Nil
+    //   // )
+    //   //   .asExpr
+    //   // }
+// // TypeDef('{}.asTerm)(name.toTypeName, Template.('{}.asTerm)(constr, parents, derived = Nil, selfOpt.getOrElse(tpd.EmptyValDef), body))
+    //   // ${New.copy('{def foo = 1}.asTerm.symbol.tree)(TypeTree.of[T]).asExpr}
+    //   // new Anon
+    //   ()
+    // }
+
+    // println(x.asTerm)
+    // println(x.asTerm.show)
+
+    ???
+  }
 }
+
+trait AResult[Warum] {
+}
+
+trait Upper {
+  def theonlyValidForNowInherited: AResult[Int]
+}
+
+trait Test extends Upper {
+  def foo: Int
+  def fooWith(): Int
+  def foobar(s:String): Int
+  def heinz: String
+
+  def NOHeinz: Int = 1
+
+  def theonlyValidForNow: AResult[Int]
+  // def theonlyValidForNow2ButNotAbstract: AResult[Int] = null
+
+  def generic[T]: Int
+
+  private def NO: Int = 1
+  protected def NO2:Int
+  private[test] def NO3 :Int
+}
+
+//     TypeDef(
+//       $anon,
+//       Template(
+//         DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),
+//         List(Apply(Select(New(Ident(AnyRef)),<init>),List())),
+//         ValDef(_,EmptyTree,EmptyTree),
+//         List(DefDef(foobar,List(),Ident(Int),Literal(Constant(1))))
+//       )
+//     )
+// Block(
+//   List(TypeDef(Boo,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),List(TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)]),ValDef(_,EmptyTree,EmptyTree),List()))),
+//   Block(List(TypeDef($anon,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)]),<init>),List()), Ident(Boo)),ValDef(_,EmptyTree,EmptyTree),List()))),Typed(Typed(Apply(Select(New(Ident($anon)),<init>),List()),TypeTree[TypeRef(NoPrefix,trait Boo)]),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)])))
+
+
+// Block(
+//   List(
+//     // TypeDef(Foos,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),List(TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)]),ValDef(_,EmptyTree,EmptyTree),List())),
+//     TypeDef(Bars,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class <root>)),object scala),Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(TermRef(ThisType(TypeRef(NoPrefix,module class java)),object lang),Object)]),<init>),List()), Ident(Foos)),ValDef(_,EmptyTree,EmptyTree),List()))
+//   ),
+//   Literal(Constant(()))
+// )
