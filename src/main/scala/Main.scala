23c5d1e67a1bb92fdf27a2a6f119ff1ed8941a36
johannes karoff <johannes@karoff.net>
2023-05-03 01:56:58 +0200

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,16 +3,22 @@ package test
 import scala.annotation.experimental
 
 trait Command {
-  def run(): Int
+  def run(bar: String): Int
   def run2(foo: String): Int
-  def run3: Int
+  def run3(bar: String): Int
 }
 
 @main
 @experimental
 def main: Unit = {
-  val myCommand: Command = MyMacro.client[Command, Int](() => 12)
-  println(myCommand.run()) // 12
-  println(myCommand.run2("test")) // 12
-  println(myCommand.run3) // 12
+  val myCommand: Command = MyMacro.client[Command, String, Int](str => str.toInt + 1)
+  println(myCommand.run("11")) // 12
+  println(myCommand.run2("2")) // 3
+  println(myCommand.run3("1")) // 2
+
+  val myRouter: Map[String, String => Int] = MyMacro.router[Command, String, Int](myCommand)
+
+  println(myRouter("run")("11")) // 12
+  println(myRouter("run2")("2")) // 3
+  println(myRouter("run2")("1")) // 2
 }
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -4,10 +4,6 @@ import scala.annotation.experimental
 import scala.language.experimental
 import scala.quoted.*
 
-trait Thing {
-  type Type
-}
-
 @experimental
 object MyMacro {
 
@@ -52,9 +48,9 @@ object MyMacro {
     } yield member
   }
 
-  transparent inline def client[T, R](r: () => R): T = ${MyMacro.clientImpl[T, R]('r)}
+  transparent inline def client[T, I, R](r: I => R): T = ${MyMacro.clientImpl[T, I, R]('r)}
 
-  def clientImpl[T: Type, R: Type](r: Expr[() => R])(using Quotes): Expr[T] = {
+  def clientImpl[T: Type, I: Type, R: Type](r: Expr[I => R])(using Quotes): Expr[T] = {
     import quotes.reflect.*
 
     val apiType = TypeRepr.of[T]
@@ -81,17 +77,55 @@ object MyMacro {
           // nullary methods
           val methodType = if (clauses.isEmpty) ByNameType(tpeRepr) else MethodType(names)(_ => tpes, _ => tpeRepr)
           Symbol.newMethod(cls, name, methodType, flags = Flags.EmptyFlags /*TODO: method.flags */, privateWithin = method.privateWithin.fold(Symbol.noSymbol)(_.typeSymbol))
-
         case _ =>
           report.errorAndAbort(s"Cannot detect type of method: ${method.name}")
       }
     }
 
     val cls = Symbol.newClass(Symbol.spliceOwner, className, parents.map(_.tpe), decls, selfType = None)
-    val body = cls.declaredMethods.map { method => DefDef(method, argss => Some('{${r}()}.asTerm)) }
+    val body = cls.declaredMethods.map { method => DefDef(method, argss => Some('{${r}(${argss(0)(0).asExprOf[I]})}.asTerm)) }
     val clsDef = ClassDef(cls, parents, body = body)
     val newCls = Typed(Apply(Select(New(TypeIdent(cls)), cls.primaryConstructor), Nil), TypeTree.of[T])
     val result = Block(List(clsDef), newCls).asExprOf[T]
+    println(result.show)
+    result
+  }
+
+  transparent inline def router[T, I, R](r: T): Map[String, String => R] = ${MyMacro.routerImpl[T, I, R]('r)}
+
+  def routerImpl[T: Type, I: Type, R: Type](impl: Expr[T])(using Quotes): Expr[Map[String, String => R]] = {
+    import quotes.reflect.*
+
+    val apiType = TypeRepr.of[T]
+    val tree = TypeTree.of[T]
+
+    val methods = definedMethodsInType[T]
+    val invalidMethods = methods.flatMap(checkMethod[R])
+    if (invalidMethods.nonEmpty) {
+      report.errorAndAbort(s"Invalid methods: ${invalidMethods.mkString(", ")}")
+    }
+
+
+    val methodDefinitions = methods.map { symbol =>
+        val lambdaType = MethodType(List("arg1"))(
+        _ => List(TypeRepr.of[String]),
+        _ => TypeRepr.of[Int],
+        )
+        
+        val lambda = Lambda(Symbol.spliceOwner, lambdaType, { (_, args) =>
+          val terms = args.map(_.asExpr.asTerm)
+          Apply(Select(impl.asTerm, symbol), terms)
+        })
+
+      '{
+        (${Expr(symbol.name)}, ${lambda.asExprOf[String => R]})
+      }
+    }
+
+    val result = '{
+      Map.from(${Expr.ofList(methodDefinitions)})
+    }
+
     println(result.show)
     result
   }
